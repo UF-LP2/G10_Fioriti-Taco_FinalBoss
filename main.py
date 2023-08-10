3b14044a7a2f9753e10ca7dbafd88f6a168cf81c
Tacostefano <tacostefano@gmail.com>
2023-08-10 17:52:15 -0300

    Forma correcta de importacion

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from src import cCruise
+from src.cCruise import Cruise
+from src.Cargo import Cargo
 
 
 def main() -> None:
-  print("Hello World")
+    print("Hello World")
+    titanic = Cruise(4, 4, 4)
+    bote = Cargo(4, 1, 4, 6)
 
 if __name__ == "__main__":
   main()
