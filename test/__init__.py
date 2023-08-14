e3fdfc4723c6eaffabff8ab2a922147dff05af58
Tacostefano <tacostefano@gmail.com>
2023-08-14 20:22:07 -0300

    Inicio de unit test

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,22 @@
 from src.cCruise import Cruise
 from src.Cargo import Cargo
 from src.Ship import Ship
-def test_is_worth_it():
-    crucero = Cruise(172, 2198, 488)
-    assert crucero.is_worth_it() == "Barco robado"
+import unittest
+
+class Test1(unittest.TestCase):
+    def test_is_worth_it(self):
+        crucero = Cruise(172, 2198, 488)
+        assert crucero.is_worth_it() == "Barco robado"
+
+class Test2(unittest.TestCase):
+    def test_ceros(self):
+        barco = Cargo(0, 0, 0, 0)
+        barco.is_worth_it()
+        self.assertRaises(ValueError,"peso menor a 20, no asaltar")
+
+
+
+class Test3(unittest.TestCase):
+    def test_negativos(self):
+        nave = Ship(-1, -5)
+        assert nave.is_worth_it() == "peso menor a 20, no asaltar"
