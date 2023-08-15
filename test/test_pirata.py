5c4013375afbac5a715c20bc0f83f90882675622
Fedefioriti <ffioriti10@gmail.com>
2023-08-15 11:35:11 -0300

    pytest terminado, agregar archivo del rey pirata

--- test/test_pirata.py
+++ test/test_pirata.py
@@ -3,16 +3,16 @@ from src.Cargo import Cargo
 from src.Ship import Ship
 import pytest
 
-def Test_worth_it():
+def test_worth_it():
     crucero = Cruise(172, 2198, 488)
     assert crucero.is_worth_it() == "Barco robado"
 
-def Test_ceros():
+def test_ceros():
     barco = Cargo(0, 0, 0, 0)
     with pytest.raises(ValueError):
         barco.is_worth_it()
 
-def Test_negativos():
+def test_negativos():
     nave = Ship(-1, -5)
     with pytest.raises(ValueError):
         nave.is_worth_it()
