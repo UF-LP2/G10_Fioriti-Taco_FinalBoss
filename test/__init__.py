9cfd4fb72405d36ac582e80440d2a3af33a54782
Fedefioriti <ffioriti10@gmail.com>
2023-08-15 10:38:06 -0300

    .

--- test/__init__.py
+++ test/__init__.py
@@ -1,20 +1,16 @@
 from src.cCruise import Cruise
 from src.Cargo import Cargo
 from src.Ship import Ship
-import unittest
+import pytest
 
-class Test1(unittest.TestCase):
-    def test_is_worth_it(self):
-        crucero = Cruise(172, 2198, 488)
-        assert crucero.is_worth_it() == "Barco robado"
+def Test_worth_it():
+    crucero = Cruise(172, 2198, 488)
+    assert crucero.is_worth_it() == "Barco robado"
 
-class Test2(unittest.TestCase):
-    def test_ceros(self):
-        barco = Cargo(0, 0, 0, 0)
+def Test_ceros():
+    barco = Cargo(0, 0, 0, 0)
+    with pytest.raises(Exception):
         barco.is_worth_it()
-        self.assertRaises(ValueError,"peso menor a 20, no asaltar")
-
-
 
 class Test3(unittest.TestCase):
     def test_negativos(self):
